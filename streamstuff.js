0201ad1b483e2cc9b778b9d004cd0bbfe3403124
Mike OShea <oshea00@gmail.com>
2013-03-22 19:10:22 -0400

    added a bit

--- streamstuff.js
+++ streamstuff.js
@@ -18,7 +18,11 @@ console.log('sdout is writeable? '+process.stdout.writable);
 
 request('http://www.pluralsight.com/').pipe(process.stdout);
 
+
 // gzip - oooh   zcat command too!
+request('http://www.pluralsight.com/')
+    .pipe(fs.createWriteStream('pluralsight.html'));
+    
 request('http://www.pluralsight.com/')
     .pipe(zlib.createGzip())
     .pipe(fs.createWriteStream('pluralsight.html.gz'));
